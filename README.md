7c1f3e1a80d36b18e3c90c55819a53992cc1a70f
Wan Bao <ylxbsoftware@163.com>
2016-07-29 23:11:19 +0800

    add gulp plugins

---
+++ README.md
@@ -0,0 +1,11 @@
+#### use gulp plugins:
+
+- gulp-uglify
+- pump
+- gulp-cssnano
+- gulp-imagemin
+- gulp-minify-html
+- gulp-ssh
+- gulp-rev
+- gulp-rev-collector
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,73 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var pump = require('pump');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
-var htmlmin = require('gulp-htmlmin');
+var minifyHTML = require('gulp-minify-html');
+var GulpSSH = require('gulp-ssh');
+var rev = require('gulp-rev');
+var revCollector = require('gulp-rev-collector');
 
 var config = {
-    root: 'dist'
+  root: 'dist'
 };
 
-gulp.task('js', function() {
-    return gulp.src('./js/*.js')
-        .pipe(uglify({
-            compress: {
-                drop_console: true
-            }
-        }))
-        .pipe(gulp.dest(config.root + '/js'));
+var sshConfig = {
+  host: '192.168.1.105',
+  username: 'wanbao',
+  password: 'wanbao'
+};
+
+var gulpSSH = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: sshConfig
 });
 
-gulp.task('html', function() {
-    return gulp.src('./*.html')
-        .pipe(htmlmin())
-        .pipe(gulp.dest(config.root + '/'))
+
+gulp.task('js', function(cb) {
+  pump([gulp.src('./js/*.js'),
+      rev(),
+      uglify(),
+      gulp.dest(config.root + '/js'),
+      rev.manifest(),
+      gulp.dest('rev/js')
+    ],
+    cb);
+});
+gulp.task('css', function() {
+  return gulp.src('./css/*.css')
+    .pipe(rev())
+    .pipe(cssnano())
+    .pipe(gulp.dest(config.root + '/css'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/css'));
 });
 
 gulp.task('image', function() {
-    return gulp.src('./images/*.*')
-        .pipe(imagemin())
-        .pipe(gulp.dest(config.root + '/images/'));
+  return gulp.src('./images/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest(config.root + '/images/'));
 });
 
-gulp.task('css', function() {
-    return gulp.src('./css/*.css')
-        .pipe(cssnano())
-        .pipe(gulp.dest(config.root + '/css'));
+gulp.task('html', function() {
+  gulp.src(['rev/**/*.json', './*.html'])
+    .pipe(revCollector({
+      replaceReved: true,
+      dirReplacements: {
+        'css': '/dist/css',
+        '/js/': '/dist/js/'
+      }
+    }))
+    .pipe(minifyHTML({
+      empty: true,
+      spare: true
+    }))
+    .pipe(gulp.dest(config.root + '/'))
 });
 
-gulp.task('default', ['js', 'html', 'css', 'image']);
+gulp.task('dest', ['js', 'css', 'image', 'html'], function() {
+  return gulp.src('/dist')
+    .pipe(gulpSSH.dest('/web'));
+});
+
+gulp.task('default', ['dest']);
